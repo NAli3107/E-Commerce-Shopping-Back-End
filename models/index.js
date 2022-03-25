141758180cff9ac5ea285c29b19489bc31d5f4d4
Nadira Ali <nadiraali188a@gmail.com>
2022-03-25 19:57:55 +0000

    belongs-to-many associations amended

--- models/index.js
+++ models/index.js
@@ -16,15 +16,23 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: "product_id",
+  through: {
+    model: ProductTag,
+    foreignKey: "product_id",
+  },
+  // Define an alias for when data is retrieved
+  as: "tags",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: "tag_id",
-})
+  through: {
+    model: ProductTag,
+    foreignKey: "tag_id",
+  },
+  // Define an alias for when data is retrieved
+  as: 'products'
+});
 
 module.exports = {
   Product,
