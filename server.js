b38fe79147e7b6112876cdd3a197bf81e38e6db2
Nadira Ali <nadiraali188a@gmail.com>
2022-03-26 16:28:22 +0000

    synced sequelize to the database on server start

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
 
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-//   });
+const init = async () => {
+  try {
+    await sequelize.sync({ force: false });
+
+    app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
+  } catch (error) {
+    console.log(`Error: Failed to connect to database - ${error.message}`);
+  }
+};
+
+init();
