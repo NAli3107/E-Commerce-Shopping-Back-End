8cd11dcfa1c55ed5924745c3feda90bdf3b5aa44
Nadira Ali <nadiraali188a@gmail.com>
2022-03-26 17:08:57 +0000

    read me update

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # E-Commerce-Shopping-Back-End
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Examples](#examples)
+- [Contact Information](#contact)
+
+## Description
+
+This application is the back end of an E-Commerce site that allows users to control their products, categories and tags. The user can view all the products associated with a category and tag, the categories together with their tags and the tags with all their associated products. Furthermore, users are able to create, update and delete products, categories and tags.
+
+## Installation
+
+- node.js needs to be installed prior to use
+- open terminal
+- clone the repo: `https://github.com/NAli3107/E-Commerce-Shopping-Back-End`
+- cd into new directory
+- download JSON packages by entering `npm i`
+- create `.env` file 
+- to connect to the database run `mysql -u root -p` and enter password from .env file
+- source the schema.sql
+- seed the data by running `npm run seed`
+- enter `npm start`
+
+## Usage
+
+This application will allow users to view, create, update, and delete categories, products, and tags.
+
+```
+View video to see MySQL walk through via 
+View video to walk through of the API routes
+View video to see Postman walk through via
+```
+
+## Examples
+
+ScreenshotS of Database:
+
+![Database-Screenshot1](./assets/images/DB1.png)
+
+![Database-Screenshot1](./assets/images/DB2.png)
+
+Screenshot of Postman:
+
+![Postman-Screenshot]()
+
+## Contact
+
+Email Address: Nadiraali188a@gmail.com
+
+Linkedin Profile: [LinkedIn](https://www.linkedin.com/in/nadira-ali-09a182106/)
+
+Github: [Profile](https://github.com/NAli3107)
+
+Github: [Repo](https://github.com/NAli3107/E-Commerce-Shopping-Back-End)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
@@ -24,14 +24,14 @@ router.get('/', async (req, res) => {
     if (!productData) {
       return res.status(404).json({ message: "No products found!" });
     }
-    return res.status(200).json(productData);
+    return res.json(productData);
   } catch (error) {
-    res.status(500).json({error: "Failed to retrieve products!"});
+    res.status(500).json({ error: "Failed to retrieve products!" });
   }
 });
 
 // get one product
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
@@ -49,16 +49,20 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!productData) {
-      return res.status(404).json({ message: "No product with that ID found!" });
+      return res
+        .status(404)
+        .json({ message: "No product with that ID found!" });
     }
     return res.json(productData);
   } catch (error) {
-    return res.status(500).json({ error: "Failed to retrieve product with this ID!" });
+    return res
+      .status(500)
+      .json({ error: "Failed to retrieve product with this ID!" });
   }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -90,7 +94,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -131,23 +135,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
   try {
     const productData = await Product.destroy({
       where: {
-        id: req.params.id
-      }
+        id: req.params.id,
+      },
     });
 
     if (!productData) {
-      res.status(404).json({ message: 'No product found with this id!' });
+      res.status(404).json({ message: "No product found with this id!" });
       return;
     }
 
     res.status(200).json(productData);
   } catch (error) {
-    res.status(500).json({error: "Failed to delete product!"});
+    res.status(500).json({ error: "Failed to delete product!" });
   }
 });
 
